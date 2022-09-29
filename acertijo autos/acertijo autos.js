738c9a8445d44a956b927ae64b4f38e5bc5aed99
josmiguelmm <miguelmendezm0802@gmail.com>
2022-09-29 01:54:55 -0500

    Arreglando el acertijo autos, pistas completas, falta el css

---
+++ acertijo autos/acertijo autos.js
@@ -0,0 +1,12 @@
+
+//programar el acertijo de los autos
+//programar el select de id car1
+
+function carro1(){
+    let car1 = document.getElementById('car1');
+    let verificar = car1.value;
+    if(verificar){
+        consol.alert("Correcto");
+    }
+}
+
