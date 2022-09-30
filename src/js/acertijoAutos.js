9766fb14d23d29d06ea98a8699a4c343277c730a
zamii <sergioazm98@gmail.com>
2022-09-30 02:43:44 -0500

    Archivos renombrados, orden de carpetas cambiado, adiciónde nuevosa archivos

---
+++ src/js/acertijoAutos.js
@@ -0,0 +1,50 @@
+function carro() {
+  
+    let counter = 0; // Counter is reset at each execution
+
+    let car1 = document.getElementById("car1").value;
+    let car2 = document.getElementById("car2").value;
+    let car3 = document.getElementById("car3").value;
+    let car4 = document.getElementById("car4").value;
+    let car5 = document.getElementById("car5").value;
+    let car6 = document.getElementById("car6").value;
+    let car7 = document.getElementById("car7").value;
+    let car8 = document.getElementById("car8").value;
+    let spanMarca = document.getElementById("marca1");
+    
+    if (car1 == "Shadow") {
+        counter++;
+        console.log(counter)
+    } if (car2 == "McLaren") {
+        counter++;
+        console.log(counter);
+    } if (car3 == "March") {
+        counter++;
+        console.log(counter);
+    } if (car4 == "Ferrari") {
+        counter++;
+        console.log(counter);
+    } if (car5 == "Lola") {
+        counter++;
+        console.log(counter);
+    } if (car6 == "Lotus") {
+        counter++;
+        console.log(counter);
+    } if (car7 == "Iso") {
+        counter++;
+        console.log(counter);
+    } if (car8 == "Tyrrell") {
+        counter++;
+        console.log(counter);
+    } else {
+        spanMarca.innerHTML = "Estas cerca " + counter + "/8";
+    }
+    //  para finalizar
+    if (counter == 8) {
+        spanMarca.innerHTML = "Felicidades, has resuelto el acertijo, CULONA <3";
+    }
+}
+
+// todos los derechos para novoa, romero, cousin, sergei, y jose.
+//  en colaboración con D1N0C0D3 
+
